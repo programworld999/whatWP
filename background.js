67dd7bad0437e725c4a9a3f0f96cc31ae6448d1f
programworld999 <programworld999@gmail.com>
2020-06-24 05:30:03 +0530

    api init success with rendering

--- background.js
+++ background.js
@@ -1,4 +1,7 @@
 window.whatWP = {};
+window.whatWP["isWPSite"] = false;
+window.whatWP["theme"] = "Unknown";
+window.whatWP["plugins"] = {};
 
 chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
   var url = "https://whatwpapi.herokuapp.com/api/?site=";
@@ -9,8 +12,20 @@ chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
   fetch(api)
     .then((r) => r.text())
     .then((result) => {
-      console.log("data: ", result);
-      // Result now contains the response text, do what you want...
+      var data = JSON.parse(result);
+      console.log(data[0].isWPSite);
+      console.log(data[1].theme);
+      console.log(data[2].plugins);
+      window.whatWP["isWPSite"] = data[0].isWPSite;
+      window.whatWP["theme"] = data[1].theme;
+      window.whatWP["plugins"] = data[2].plugins;
+      if (window.whatWP["isWPSite"]) {
+        chrome.browserAction.setIcon({
+          path: "assets/icons/wpsite/icon-144x144.png",
+          tabId: sender.tab.id,
+        });
+      } else {
+      }
     });
   sendResponse({ res: window.site });
   return true; // Will respond asynchronously.
